d579ce928167663a4c2a31707036c088686ba0e8
DeltaDev <achyut.kautilya@gmail.com>
2025-05-28 17:25:32 +0100

    Initial deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// Change 'YOUR-USERNAME' and 'YOUR-REPO' to match your GitHub username/repo
 export default defineConfig({
   plugins: [react()],
+  base: '/SpaceShield/', // <-- use your repo name here, with slashes
 })
